bbfee7af228c2aa0c775997bd72e797631c68d09
evelientje90 <evelina.kupryte1990@gmail.com>
2022-02-13 11:56:35 +0100

    Search Engine inserted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
         <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
-        <footer className="text-center">Coded by SheCodes
-        </footer>
+        	<header className="App-header">
+          	<img src={logo} className="App-logo img-fluid" alt="logo" />
+        	<main>
+				<Dictionary />
+			</main>
+			</header>
+        	<footer className="text-center">Coded by SheCodes
+        	</footer>
       </div>
     </div>
   );
