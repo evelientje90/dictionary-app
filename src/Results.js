38bfb129f05867e273f484ed41aa3dee6ffdce02
evelientje90 <evelina.kupryte1990@gmail.com>
2022-02-22 08:31:59 +0100

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -6,21 +6,23 @@ export default function Results(props) {
     if (props.results) {
     return (
     <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function(phonetic, index) {
-        return (
-            <div key={index}>
+        <section>
+            <h2>{props.results.word}</h2>
+            {props.results.phonetics.map(function(phonetic, index) {
+            return (
+                <div key={index}>
                 <Phonetic phonetic={phonetic} />
-            </div>
-        );
-        })}
-        {props.results.meanings.map(function(meaning, index) {
-        return (
-            <div key={index}>
+                </div>
+                );
+                })}
+        </section>
+            {props.results.meanings.map(function(meaning, index) {
+            return (
+                <section key={index}>
                 <Meaning meaning={meaning} />
-            </div>
-        );
-        })}
+                </section>
+                );
+            })}
     </div>
     );
 } else {
